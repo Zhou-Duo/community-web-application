51652c2c1058a255ec4322a26f9e577cad1afa78
Zhou-Duo <xzhouaz@connect.ust.hk>
2022-11-24 01:01:14 -0500

    Cookie, Session Test + Kaptcha verification

--- src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -1,14 +1,17 @@
 package com.nowcoder.community.controller;
 
 import com.nowcoder.community.service.AlphaService;
+import com.nowcoder.community.util.CommunityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.*;
@@ -124,7 +127,7 @@ public class AlphaController {
     // DispatcherServlet 调用函数时，检测到 @ResponseBody 注解 且函数返回的是 Java 对象，会自动将返回的 Map 转化为
     // JSON 字符串
     @ResponseBody
-    public Map getEmp() {
+    public Map<String, Object> getEmp() {
         Map<String, Object> map = new HashMap<>();
         map.put("name", "Minyue");
         map.put("age", "23");
@@ -149,6 +152,48 @@ public class AlphaController {
         emp2.put("salary", "30000");
         list.add(emp2);
         return list;
+    }
+
+    // cookie示例
+
+    @RequestMapping(path = "/cookie/set", method = RequestMethod.GET)
+    @ResponseBody
+    public String setCookie(HttpServletResponse response) {
+        // 创建cookie
+        Cookie cookie = new Cookie("code", CommunityUtil.generateUUID());
+        // 设置cookie生效的范围
+        cookie.setPath("/community/alpha");
+        // 设置cookie的生存时间
+        cookie.setMaxAge(60 * 10);
+        // 发送cookie
+        response.addCookie(cookie);
+
+        return "set cookie";
+    }
+
+    @RequestMapping(path = "/cookie/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getCookie(@CookieValue("code") String code) {
+        System.out.println(code);
+        return "get cookie";
+    }
+
+    // session示例
+
+    @RequestMapping(path = "/session/set", method = RequestMethod.GET)
+    @ResponseBody
+    public String setSession(HttpSession session) {
+        session.setAttribute("id", 1);
+        session.setAttribute("name", "Test");
+        return "set session";
+    }
+
+    @RequestMapping(path = "/session/get", method = RequestMethod.GET)
+    @ResponseBody
+    public String getSession(HttpSession session) {
+        System.out.println(session.getAttribute("id"));
+        System.out.println(session.getAttribute("name"));
+        return "get session";
     }
 
 }
