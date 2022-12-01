a8e7e33cc2d6d9b846390952a52dc73ec3115790
Zhou-Duo <xzhouaz@connect.ust.hk>
2022-12-01 01:20:46 -0500

    add new discuss post

--- src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -196,4 +196,12 @@ public class AlphaController {
         return "get session";
     }
 
+    // ajax示例
+    @RequestMapping(path = "/ajax", method = RequestMethod.POST)
+    @ResponseBody
+    public String testAjax(String name, int age) {
+        System.out.println(name);
+        System.out.println(age);
+        return CommunityUtil.getJSONString(0, "操作成功!");
+    }
 }
