0280c6458cc372a445f24c53e03efe30105967dc
Zhou-Duo <xzhouaz@connect.ust.hk>
2022-12-01 00:07:17 -0500

    Sensitive words filter

---
+++ src/test/java/com/nowcoder/community/SensitiveTests.java
@@ -0,0 +1,28 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.SensitiveFilter;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class SensitiveTests {
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Test
+    public void testSensitiveFilter() {
+        String text = "这里可以赌博、可以嫖娼、可以吸毒、可以开票，哈哈哈！";
+        text = sensitiveFilter.filter(text);
+        System.out.println(text);
+
+        text = "这里可以😄赌😄博😄、可以😄嫖😄娼😄、可以😄吸😄毒😄、可以😄开😄票😄，哈哈哈！";
+        text = sensitiveFilter.filter(text);
+        System.out.println(text);
+    }
+}
